64ece179b57b969dbc02d5ab50f3779d9da84cff
Connor Holly <connorholly11@gmail.com>
2019-10-04 13:02:34 -0500

    api working

--- apichallengestretch/src/App.js
+++ apichallengestretch/src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [project, setProject] = useState([]);
+
+  // axios.get("http://localhost:3000/project").then(res => {
+  //   console.log(res);
+  //   console.log(res.data);
+  //   setProject(res.data);
+  // });
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/project").then(res => {
+      console.log(res);
+      console.log(res.data);
+      setProject(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>HELLO REACT APP</h1>
+    <div>
+      <div className="App">
+        <h1>HELLO REACT APP</h1>
+      </div>
+      <div>
+        {project.map(projects => (
+          <div>
+            <h1>{projects.name}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
